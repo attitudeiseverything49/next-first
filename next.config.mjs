467303712c5a16d827cf49bcb65d0753453dd163
MohamedShahulHameedRiyaz <MohamedShahul.HameedRiyaz@ltimindtree.com>
2025-12-09 10:13:41 +0530

    commit

--- next.config.mjs
+++ next.config.mjs
@@ -3,6 +3,9 @@ import next from 'next'
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
+    images: {
+    unoptimized: true,
+  },
  
   // Optional: Change the output directory `out` -> `dist`
   // distDir: 'dist',
