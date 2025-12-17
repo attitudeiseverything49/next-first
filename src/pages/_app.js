e58e70d0941b0a148ca15d934ef3d4c1dc06e72d
attitudeiseverything49 <79516849+attitudeiseverything49@users.noreply.github.com>
2025-12-17 19:06:27 +0530

    Integrate Google Tag Manager in the app
    Added Google Tag Manager component to the app.

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,9 @@
 import "@/styles/globals.css";
+import { GoogleTagManager } from '@next/third-parties/google'
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return <>
+    <Component {...pageProps} /> 
+    <GoogleTagManager gtmId="GTM-KWP98NS8"/>
+    </>
 }
